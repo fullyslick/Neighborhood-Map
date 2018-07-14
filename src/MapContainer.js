de9ed4fd3015325eb5d9fd9302c03a85b8a73453
fullyslick <alexander.rashkov.alfa@gmail.com>
2018-07-14 15:51:00 +0300

    Display basic map

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,9 +1,25 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+// import GoogleApiWrapper Higher-Order component
+// which will configure the request to Google Maps API
+import {GoogleApiWrapper, Map, Marker} from 'google-maps-react'
 
-class MapContainer extends Component {
-  render() {
-    return (<div>Map Container</div>)
+export class MapContainer extends React.Component {
+  render(){
+    return(
+      <div>
+        <Map
+         google={this.props.google}
+         initialCenter={{
+           lat: 40.854885,
+           lng: -88.081807
+         }}
+         zoom={15}
+       />
+      </div>
+    )
   }
 }
 
-export default MapContainer;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyA8Zg1KCtmv_PZCsR_BhgIYmV4sep78eyw"
+})(MapContainer)
