0389d5cef5dc45952de53c392e8e19e23129f1c0
fullyslick <alexander.rashkov.alfa@gmail.com>
2018-07-15 16:53:18 +0300

    Filter changes options

--- src/Filter.js
+++ src/Filter.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
 
 class Filter extends Component{
+
+  state = {
+    // Holds the value selected in the options
+    value: "all"
+  }
+
+  // Displays the category in the option,
+  // calls a method on the master MapContainer to change category,
+  // which cause showing only to places from selected category
+  handleChange = (event) => {
+    this.setState({value: event.target.value})
+  }
+
   render(){
     return(
-      <div className="filter">Filter</div>
+      <label>
+         Category:
+         <select value={this.state.value} onChange={this.handleChange}>
+           <option value="all">All</option>
+           <option value="sights">Sights</option>
+           <option value="bars">Bar</option>
+           <option value="restaurants">Restaurants</option>
+         </select>
+       </label>
     )
   }
 }
