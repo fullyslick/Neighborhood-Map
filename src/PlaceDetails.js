5ed76418200b3e226356c4a37a0550e37728e4ab
fullyslick <default@mail.com>
2018-07-16 11:42:34 +0300

    Make fetch request to foursquare

--- src/PlaceDetails.js
+++ src/PlaceDetails.js
@@ -9,10 +9,32 @@ class PlaceDetails extends Component {
     closeDetails: PropTypes.func.isRequired
   }
 
+  // Fetch the place's details from foursquare
+  fetchDetails = (placeId) => {
+    const clientId = "2W33ALUML5UVZMFH5UWT0MLMQWXFJCRIICNCHAVADOK4FAUP"
+    const clientSecret = "XP5APABV2VN1ZVRVTAIKORAJVHGY4Y5BJIUHUBGOEKQ51JBF"
+    const version = 20180716
+    const url = `https://api.foursquare.com/v2/venues/${placeId}?&client_id=${clientId}&client_secret=${clientSecret}&v=${version}`
+
+    fetch(url).then(response => {
+      if (!response.ok) {
+        console.log("error occured");
+      }
+      return response.json()
+    }).then((json) => {
+      console.log(json)
+    }).catch(error => console.log("Error is " + error))
+  }
+
   // Closes the details view,
   // By calling closeDetails on master MapContainer
   closeDetails = () => {
     this.props.closeDetails();
+  }
+
+  // When component is mounted fecth the details about the place
+  componentDidMount = () => {
+    this.fetchDetails(this.props.placeId);
   }
 
   render() {
